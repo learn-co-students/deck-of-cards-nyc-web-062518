e71ead5247d4ea57d8b290fd88ee5e7e8b799377
seanraad123 <seanconrad123@gmail.com>
2018-07-09 10:02:24 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,31 @@
+require 'pry'
+
 class Deck
 
+	attr_accessor :cards
+
+	def initialize
+		@rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+		@suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+		@cards = 52.times.map {Card.new(@rank.sample, @suit.sample)}
+	end
+
+	def choose_card
+		@cards.pop
+	end
 end
 
 class Card
 
+	attr_accessor :suit, :rank
+
+	def initialize(suit, rank)
+		@suit = suit
+		@rank = rank
+	end
+
+
 end
+
+
+
